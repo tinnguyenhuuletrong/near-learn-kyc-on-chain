825ff4007c049d3ad47de792f8c8b95de771e031
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-09-14 15:33:33 +0700

    test

--- deno-deploy/local.ts
+++ deno-deploy/local.ts
@@ -1,22 +1,46 @@
-import * as NEARAPI from "./lib/near-api-js.min.js";
+import * as nearAPI from "https://dev.jspm.io/near-api-js";
+// import * as nearAPI from "https://jspm.dev/near-api-js";
+
+const nearApi = nearAPI.default as any;
+const { connect, keyStores } = nearApi;
+const config = {
+  networkId: "testnet",
+  keyStore: new keyStores.InMemoryKeyStore(),
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  explorerUrl: "https://explorer.testnet.near.org",
+};
+const ACC_ID = "blockpass-kyc-onchain-alpha.ttintest.testnet";
+const PRIVATE_KEY =
+  "2go3cuvBJp3DQ4Rxwmt1SZN6Y2VcJVDkFjgfbPKP2nDJ8eLUygg6gk9oDvEkFtyqzVaKmDALENKrUdBoRwodDSC9";
+const CONTRACT_ID = "dev-1631519031086-9364402";
+
 async function main() {
-  let tmp = NEARAPI;
-  const nearAPI = (window as any).nearApi;
-  const { connect, keyStores, WalletConnection } = nearAPI;
-  const config = {
-    networkId: "testnet",
-    keyStore: new keyStores.InMemoryKeyStore(),
-    nodeUrl: "https://rpc.testnet.near.org",
-    walletUrl: "https://wallet.testnet.near.org",
-    helperUrl: "https://helper.testnet.near.org",
-    explorerUrl: "https://explorer.testnet.near.org",
-  };
   // connect to NEAR
   const near = await connect(config);
-  const account: NEARAPI.Account = await near.account(
-    "example-account.testnet"
+
+  // load keys
+  const keyPair = new nearApi.utils.key_pair.KeyPairEd25519(PRIVATE_KEY);
+  await config.keyStore.setKey(config.networkId, ACC_ID, keyPair);
+
+  // create sc
+  const account = await near.account(ACC_ID);
+  const contract = new nearApi.Contract(
+    account, // the account object that is connecting
+    CONTRACT_ID,
+    {
+      // name of contract you're connecting to
+      changeMethods: ["updateCandidateStatus"], // change methods modify state
+      sender: account, // account object to initialize and sign transactions.
+    }
   );
-  console.log(await account.getAccountBalance());
-  console.log(await account.getAccessKeys());
+
+  console.log(
+    await contract.updateCandidateStatus({
+      refId: "ECz4McR81TkgxhqssRhqM88ahAGJHR8GMaTdJxqgKXT1",
+      status: "1",
+    })
+  );
 }
 main();
--- deno-deploy/mod.ts
+++ deno-deploy/mod.ts
@@ -1,34 +1,56 @@
-import * as NEARAPI from "./lib/near-api-js.min.js";
+import * as nearAPI from "https://dev.jspm.io/near-api-js";
+
+const nearApi = nearAPI.default as any;
+const { connect, keyStores } = nearApi;
+const config = {
+  networkId: "testnet",
+  keyStore: new keyStores.InMemoryKeyStore(),
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  explorerUrl: "https://explorer.testnet.near.org",
+};
+const ACC_ID = "blockpass-kyc-onchain-alpha.ttintest.testnet";
+const PRIVATE_KEY =
+  "2go3cuvBJp3DQ4Rxwmt1SZN6Y2VcJVDkFjgfbPKP2nDJ8eLUygg6gk9oDvEkFtyqzVaKmDALENKrUdBoRwodDSC9";
+const CONTRACT_ID = "dev-1631519031086-9364402";
+
 async function handleRequest(request: Request) {
-  let dummy = NEARAPI;
-
-  const nearAPI = (window as any).nearApi;
-  const accName = "example-account.testnet";
-
-  const { connect, keyStores, WalletConnection } = nearAPI;
-
-  const config = {
-    networkId: "testnet",
-    keyStore: new keyStores.InMemoryKeyStore(),
-    nodeUrl: "https://rpc.testnet.near.org",
-    walletUrl: "https://wallet.testnet.near.org",
-    helperUrl: "https://helper.testnet.near.org",
-    explorerUrl: "https://explorer.testnet.near.org",
-  };
-
   // connect to NEAR
   const near = await connect(config);
-  const account: NEARAPI.Account = await near.account(accName);
-  const balance = await account.getAccountBalance();
-  const keys = await account.getAccessKeys();
+
+  // load keys
+  const keyPair = new nearApi.utils.key_pair.KeyPairEd25519(PRIVATE_KEY);
+  await config.keyStore.setKey(config.networkId, ACC_ID, keyPair);
+
+  // create sc
+  const account = await near.account(ACC_ID);
+  const contract = new nearApi.Contract(
+    account, // the account object that is connecting
+    CONTRACT_ID,
+    {
+      // name of contract you're connecting to
+      changeMethods: ["updateCandidateStatus"], // change methods modify state
+      sender: account, // account object to initialize and sign transactions.
+    }
+  );
+
+  // console.log(
+  //   await contract.updateCandidateStatus({
+  //     refId: "ECz4McR81TkgxhqssRhqM88ahAGJHR8GMaTdJxqgKXT1",
+  //     status: "1",
+  //   })
+  // );
 
   // create wallet connection
   // const wallet = new WalletConnection(near);
+  const balance = await account.getAccountBalance();
+  const body = await request.json();
 
   const json = JSON.stringify({
-    accName,
+    accName: ACC_ID,
     balance,
-    keys,
+    body,
   });
 
   return new Response(json, {
@@ -41,6 +63,6 @@ async function handleRequest(request: Request) {
   });
 }
 
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: any) => {
   event.respondWith(handleRequest(event.request));
 });
